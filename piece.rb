dab4a82febb238d697d3fa82abe1d4447797b6c6
Adam Blum <adam.blum89@gmail.com>
2018-02-13 14:54:47 -0500

    Start implement moves

--- piece.rb
+++ piece.rb
@@ -1,9 +1,15 @@
 class Piece
   attr_reader :piece
-  def initialize(piece = nil)
+  def initialize(board, piece = nil)
     @piece = piece
-    @moves = []
+    @color = color
+    @board = board
     @curr_pos = nil
   end
+
+  def add_diffs(diff1, diff2)
+    a, b = diff1
+    c, d = diff2
+    [a + c, b + d]
+  end
 end
-  
--- sliding_piece.rb
+++ sliding_piece.rb
@@ -26,12 +26,28 @@ module SlidingPiece
 
 
   def moves(direction)
+    res = [@curr_pos]
+    if direction == :vert_horz
+      VERT_HORZ_DIFFS.each do |diff|
+        new_pos = add_diffs(@curr_pos, diff)
+        until obstacle?(new_pos)
+          if board[new_pos].color != self.color
+            res << new_pos
+            next
+          else
+            res << new_pos
+          end
+          new_pos = add_diffs(res.last, diff)
+        end
+
+
+
+
 
   end
 
-  def obstacle?
-
-
+  def obstacle?(pos)
+    board[pos].color == this.color || !board.valid_pos?(pos) ? true : false
   end
 
 end
